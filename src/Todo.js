e91a277bb788c6b5866393e15cf4b6e20c815cc7
Vivek Kumar <vickykumar7061@gmail.com>
2024-07-27 15:58:05 +0530

    Concepts of useState Added

---
+++ src/Todo.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import TodoList from "./TodoList";
+function Todo() {
+    const [todos, setTodos] = useState([]);
+    const [newTodo, setNewTodo] = useState("");
+
+    return (
+        <div>
+            <input type="text" placeholder="new todo" 
+                onChange={(event) => setNewTodo(event.target.value)}
+            /> <br />
+            <button onClick={() => setTodos([...todos,newTodo])}>Add Todo</button>
+            <TodoList todos={todos}/>
+        </div>
+    );
+}
+
+export default Todo;
